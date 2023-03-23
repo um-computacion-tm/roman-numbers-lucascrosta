0e077022a2be8e30897d6809c2b6189f488a1fcd
lucascrosta <l.crosta@alumno.um.edu.ar>
2023-03-23 16:43:18 -0300

    avanzado

--- nrosromanos.py
+++ nrosromanos.py
@@ -7,8 +7,21 @@ def decimal_to_roman(decimal):
         return "V"
     elif decimal > 5 and decimal < 9: 
         return "V" + "I"*(decimal-5)
-    else:
+    elif decimal == 50:
+        return "L"
+    elif decimal == 100:
+        return "C"
+    elif decimal == 500:
+        return "D"
+    elif decimal == 1000:
+        return "M"
+    elif decimal == 10:
         return "X"
+    elif decimal > 10 and decimal < 14:
+        return "X" + "I"*(decimal-10)
+    elif decimal > 100 and decimal < 400:
+        return "C"*(decimal//100)
+
 
 class TestDecimalToRoman(unittest.TestCase):
     def test_1(self):
@@ -41,6 +54,21 @@ class TestDecimalToRoman(unittest.TestCase):
 
     def test_8(self):
         self.assertEqual(decimal_to_roman(8), "VIII")
+
+    def test_50(self):
+        self.assertEqual(decimal_to_roman(50), "L")
+
+    def test_100(self):
+        self.assertEqual(decimal_to_roman(100), "C")
+
+    def test_500(self):
+        self.assertEqual(decimal_to_roman(500), "D")
+    
+    def test_1000(self):
+        self.assertEqual(decimal_to_roman(1000), "M")
+
+    def test_200(self):
+        self.assertEqual(decimal_to_roman(200), "CC")
 
 
 if __name__ == "__main__":
